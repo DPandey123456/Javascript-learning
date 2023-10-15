f4f67c1cfbde912a060b0f4327e481c17a5547ef
DPandey123456 <139701854+DPandey123456@users.noreply.github.com>
2023-10-15 05:12:16 +0000

    Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") we are using node js not browser 
+
+console.log("Hi What about you ?")
+
+let name = "Dinesh"
+let age = 23
+let loggedIn = true
+
+
+// type od data type
+// nuber = 2 to power of 53
+// bigint 
+// string , use is dubble coree
+//boolean  = true / false
+//null =(it is a standlone value and this is a object), it repersent a empty value
+
+// undefined => value not assigned
+// symbol => unique
+// object=>
+
+console.log(typeof "Dinesh");
+
+console.log(typeof age);
+console.log(typeof null);
